cc84e39112f19bd8b063e8b14a6590e47e48ea38
21ccronk <21ccronk@athenian.org>
2020-10-15 17:16:10 +0000

    Finished

--- my_code.py
+++ my_code.py
@@ -1,28 +1,20 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#  https://www.w3schools.com/python/python_ref_dictionary.asp
 
 def make_dict():
-    '''
-    use the following information to create a dictionary called currency:
-    keys = ['Ten', 'Twenty', 'Thirty']
-    values = [10, 20, 30]
-    '''
-    currency =          # complete this line
+    currency = {'Ten' : 10 ,'Twenty' : 20 ,'Thirty' : 30}
 
     return currency
 
 def add_element():
     foods = {'fruit': 'apple', 'veggie': 'carrot', 'grain': 'barley'}
-    # add a key value pair 'dairy': 'yogurt' to the following dictionary
-
-    # add code here
+    foods['dairy'] = 'yogurt'
 
     return foods
 
 def remove_element():
     foods = {'fruit': 'apple', 'veggie': 'carrot', 'grain': 'barley'}
-    # remove 'veggie': 'carrot' from the dictionary
-
+    del foods['veggie']
 
     return foods
 
@@ -30,23 +22,19 @@ def merge_dict():
     # Merge these two dictionaries together so the contents are in numerical order:
     dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
     dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
-    # add code here
+    dict1.update(dict2)
 
-    return     # return new dictionary
+    return dict1  
 
 def access_key():
-    # return the value of the key 'Twenty'
     currency = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+    val = currency.pop('Twenty')     
 
-    val =             # add code to assign the desired value to 'val'
     return val
-
-
-
-
 
 if __name__ == '__main__':
     # Test your code with this first
     # Change the function to test different sections
-    print(make_dict())
+    print(merge_dict())
+    
 
